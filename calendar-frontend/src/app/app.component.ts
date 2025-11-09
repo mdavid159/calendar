f481c20a744b21f8961c666225e9b1f556a7c7bf
David Muntean <davidmuntean658@gmail.com>
2025-11-09 20:17:21 +0200

    fix(frontend): title fix

--- calendar-frontend/src/app/app.component.ts
+++ calendar-frontend/src/app/app.component.ts
@@ -8,4 +8,6 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   standalone: true
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'calendar-frontend';
+}
